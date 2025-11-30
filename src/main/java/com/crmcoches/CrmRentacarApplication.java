a3f35d740c628c255f7621856825e189ef37e9ae
jaime asensio <jimy.asensio@gmail.com>
2025-11-30 17:26:21 +0100

    back crm jaime

---
+++ src/main/java/com/crmcoches/CrmRentacarApplication.java
@@ -0,0 +1,13 @@
+package com.crmcoches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmRentacarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmRentacarApplication.class, args);
+	}
+
+}
