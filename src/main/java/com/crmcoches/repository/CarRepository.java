ba1d05092c910ef0712e7421cf0e42cd4b5d11e4
jaime asensio <jimy.asensio@gmail.com>
2025-12-15 22:13:08 +0100

    creacion tabla coches con sus diferentes columnas en la base de datos y llamada a la api

---
+++ src/main/java/com/crmcoches/repository/CarRepository.java
@@ -0,0 +1,9 @@
+package com.crmcoches.repository;
+
+import com.crmcoches.entity.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarRepository extends JpaRepository<Car, Long> {
+}
---
+++ src/main/java/com/crmcoches/services/auth/admin/AdminService.java
@@ -0,0 +1,13 @@
+package com.crmcoches.services.auth.admin;
+
+import com.crmcoches.dto.CarDto;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface AdminService {
+
+    boolean postCar(CarDto carDto) throws IOException;
+
+    List<CarDto> getAllCars();
+}
